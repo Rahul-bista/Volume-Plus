a253bf94d69b81396b8933016573b04df9ff3e7d
Rauru <rahulbista850@gmail.com>
2024-07-23 19:40:12 +0545

    initial commit

---
+++ app/src/main/java/com/volumebooster/volumeplus/MainActivity.java
@@ -0,0 +1,14 @@
+package com.volumebooster.volumeplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
